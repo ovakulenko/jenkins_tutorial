0283c97fc6a7d45467be1af515148e44237146c6
Oleksandr Vakulenko <Oleksandr_Vakulenko@epam.com>
2018-12-19 11:22:37 +0200

    Hello World with Date

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,13 @@
-/* This is a simple Java program. 
+/* This is a simple Java program.
    FileName : "HelloWorld.java". */
-class HelloWorld 
-{ 
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World next iteration"); 
-    } 
-} 
+import java.util.Date;
+
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        Date date = new Date();
+        String Str = String.format("Hello World: %tc", date);
+        System.out.println(Str);
+    }
+}
